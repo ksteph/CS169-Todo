de237183c7a0185b4a925d3ba548c669034793f1
DarryQueen <slimfrog@gmail.com>
2016-09-19 20:23:43 -0700

    Step definitions for clickable checks.

--- features/step_definitions/todo_steps.rb
+++ features/step_definitions/todo_steps.rb
@@ -8,6 +8,12 @@ Given /^(?:|I )am on (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
+When /^(?:|I )click the check?(?:.*) next to "([^"]*)"$/ do |task|
+  button = page.first('tr', :text => task).first('td button')
+  assert(!button.nil?, "No clickable button found next to \"#{task}\"!")
+  button.click
+end
+
 Then /^(?:|I )should see a check?(.*) next to "([^"]*)"$/ do |check_type, task|
   check_class = nil
 
@@ -20,4 +26,13 @@ Then /^(?:|I )should see a check?(.*) next to "([^"]*)"$/ do |check_type, task|
     check_class = 'check'
   end
   assert(page.body =~ /<tr>.*#{check_class}(?:(?!<\/tr>).)*?#{task}.*?<\/tr>/m, "No check#{check_type} next to \"#{task}\"!")
+end
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
 end
